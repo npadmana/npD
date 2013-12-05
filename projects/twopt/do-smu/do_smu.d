dee2e636afe30bd8072987e6214a02177a0f66ba
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-12-04 21:34:53 -0500

    Add in support for MPI distribution of jobs

--- projects/twopt/do-smu/do_smu.d
+++ projects/twopt/do-smu/do_smu.d
@@ -1,12 +1,19 @@
-import std.stdio, std.algorithm, std.array;
+import std.stdio, std.algorithm, std.array, std.conv;
 
-import ini, spatial, paircounters;
+import mpi, ini, spatial, paircounters;
 
-void main(string[] args) {
-	if (args.length < 2) throw new Exception("do-smu.x <inifile>");
+void main(char[][] args) {
+	if (MPI_Init(args) != 0) throw new Exception("Unable to initialize MPI");
+	scope(exit) MPI_Finalize();
+	
+	//  Get rank
+	int rank,size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	// Parse the input file
-	auto ini = new IniFile(args[1]);
+	if (args.length < 2) throw new Exception("do-smu.x <inifile>");
+	auto ini = new IniFile(to!string(args[1]));
 
 	// Set parameters -- no default values to prevent confusion
 	auto ns = ini.get!int("ns");
@@ -23,11 +30,12 @@ void main(string[] args) {
 
 	// Loop over jobs
 	bool noRR;
-	foreach (job1; jobs) {
+	foreach (ijob, job1; jobs) {
+		if ((ijob % size) != rank) continue;
 		auto params = ini.get!(string[])(job1);
 		if (params.length < 3) throw new Exception("job specs need at least three parameters");
 		if ((params.length > 3) && (params[3]=="noRR")) noRR=true; else noRR=false;
-		writef("%s : Processing D=%s and D=%s to %s-{norm,DD,DR",job1, params[0],params[1],params[2]);
+		writef("[%d]%s : Processing D=%s and D=%s to %s-{norm,DD,DR",rank, job1, params[0],params[1],params[2]);
 		if (!noRR) write(",RR");
 		writeln("}.dat .....");
 	}
