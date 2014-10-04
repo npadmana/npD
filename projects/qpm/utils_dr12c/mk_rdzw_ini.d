5fe5b48d50c65ae79c569dc6e06e9b5790f13a7d
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2014-10-04 11:11:28 -0700

    Make this code more flexible

--- projects/qpm/utils_dr12c/mk_rdzw_ini.d
+++ projects/qpm/utils_dr12c/mk_rdzw_ini.d
@@ -1,22 +1,31 @@
-import std.stdio, std.string;
+import std.stdio, std.string, std.conv;
 
-void main() {
+void main(string[] s) {
+	int start = to!int(s[2]);
+	int end = to!int(s[3])+1;
+	auto gc = s[1];
+	string gc2;
+	if (gc=="ngc") {
+	  gc2 = "N";
+        } else {
+	  gc2 = "S";
+        }  
+
 	// This is the QPM cosmology
 	auto hdr = r"zmin = 0.43
 zmax = 0.7
 P0 = 20000.0
 OmegaM = 0.29
-nzfn = nzfit_dr11_vm22_north.txt
+nzfn = ../nbars/nbar-cmass-dr12v4-%s-Reid.dat 
 
 ";
-	write(hdr);
+	write(format(hdr,gc2));
 
-	auto qpmdir = r"/project/projectdirs/boss/galaxy/QPM/old/dr11b_cmass";
-	auto gc = "ngc";
+	auto qpmdir = r"/global/scratch2/sd/mwhite/QPM/DR12";
 
-	foreach (i; 1..101) {
-		auto fnhdr = format("%s/a0.6452_%04d.dr11b_%s",gc,i,gc);
-		writefln("job%03d = %s/%s.rdzw %s.xyzwi true", i, qpmdir, fnhdr, fnhdr);
+	foreach (i; start..end) {
+		auto fnhdr = format("dr12c_cmass_%s_%04d.dat",gc,i);
+		writefln("job%03d = %s/%s.rdzw %s/%s.xyzwi true", i, qpmdir, fnhdr, gc, fnhdr);
 	}
 
 }
