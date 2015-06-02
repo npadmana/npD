bdbaf35322701e51a318ffaafc479f70ac1e557a
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2015-06-02 12:56:57 -0700

    Code to write out scripts

---
+++ projects/qpm/dr12d_combined_pvp/mk_xyzwi.d
@@ -0,0 +1,21 @@
+import std.stdio,std.string;
+
+void main() {
+   auto hdr = r"#PBS -q regular
+#PBS -l mppwidth=24
+#PBS -l walltime=00:50:00
+#PBS -j oe
+#PBS -V
+
+cd $PBS_O_WORKDIR
+module load gsl
+aprun -n 1 ../exec/rdzw2xyzwi_ngc.x ini/xyzwi-ngc-%d.ini
+
+";
+
+   foreach(i;0..10) {
+	auto ff = File(format("ngc.%02d.qsub",i),"w");
+	ff.writeln(format(hdr,i));
+   }
+
+}
