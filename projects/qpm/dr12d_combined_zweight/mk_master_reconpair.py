95adb8aa508c02a433bc085952a74a8369e9b689
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2015-07-16 12:32:23 -0700

    save scripts

--- projects/qpm/dr12d_combined_zweight/mk_master_reconpair.py
+++ projects/qpm/dr12d_combined_zweight/mk_master_reconpair.py
@@ -19,8 +19,10 @@ def getfiles(inprefix, outprefix, n0, n1):
   ll = list(ifilter(toRun, ll))
   return ll
 
-def writefiles(ll, outprefix,blocksize=50):
-  count = 0
+def writefiles(ll, outprefix,blocksize=50, startcount=0):
+  if blocksize < 1 :
+    raise RuntimeError, "Blocksize should not be less than 0"
+  count = startcount
   lastpos = 0
   firstpos = 0
   while lastpos < len(ll) :
@@ -46,23 +48,42 @@ def writeqsub(template, script, n0, n1):
 
 ngcpbs="""#PBS -q regular
 #PBS -l mppwidth=1512
-#PBS -l walltime=02:00:00
+#PBS -l walltime=00:30:00
 #PBS -j oe
 #PBS -A hacc
 #PBS -o ngc.$PBS_JOBID.out
 
 cd $PBS_O_WORKDIR
-aprun -n 63 -N 1 -d 24 -cc none ../exec/smu_zweight.x -fpair.conf --jobFile=ini/recon_ngc_jobs_%02d.dat
+aprun -n 63 -N 1 -d 24 -cc none ../exec/smu_zweight.x -fpair.conf --jobFile=ini/recon_ngc_jobs_%03d.dat
 """
 
-def ngc():
+def ngc(startcount=0):
   ll = getfiles("recon_ngc/qpm_biasmock_%04d.dr12d_combined_ngc","paircounts/recon_ngc/qpm_biasmock_%04d.dr12d_combined_ngc",1,1001)
-  count = writefiles(ll,"ini/recon_ngc_jobs_%02d.dat")
-  writeqsub(ngcpbs,"scripts/recon-pair/ngc.%02d.qsub",0,count)
+  count = writefiles(ll,"ini/recon_ngc_jobs_%03d.dat",startcount=startcount, blocksize=10) # Work around OOM
+  writeqsub(ngcpbs,"scripts/recon-pair/ngc.%03d.qsub",startcount,count)
+ 
+sgcpbs="""#PBS -q regular
+#PBS -l mppwidth=1512
+#PBS -l walltime=01:00:00
+#PBS -j oe
+#PBS -A hacc
+#PBS -o sgc.$PBS_JOBID.out
+
+cd $PBS_O_WORKDIR
+aprun -n 63 -N 1 -d 24 -cc none ../exec/smu_zweight.x -fpair.conf --jobFile=ini/recon_sgc_jobs_%02d.dat
+"""
+
+def sgc(startcount=0):
+  ll = getfiles("recon_sgc/qpm_biasmock_%04d.dr12d_combined_sgc","paircounts/recon_sgc/qpm_biasmock_%04d.dr12d_combined_sgc",1,1001)
+  count = writefiles(ll,"ini/recon_sgc_jobs_%02d.dat",startcount=startcount)
+  writeqsub(sgcpbs,"scripts/recon-pair/sgc.%02d.qsub",startcount,count)
  
 
 if __name__=="__main__" :
   cap = sys.argv[1]
+  startcount = int(sys.argv[2])
   if cap == "ngc" :
-    ngc();
+    ngc(startcount);
+  elif cap == "sgc" :
+    sgc(startcount)
  
